f584aeddd2ffacc139e3acbcf9d61b12fb3057f0
JuniorMoreno <32491590+JuniorMoreno@users.noreply.github.com>
2018-12-31 10:50:00 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,21 +2,20 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-var anyVar = [1, "Junior", 3.14, true, false]
+var anyVar = [1, "Junior", 3.14, true, null ]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 function myFunction(arg){
     return arg;
-};
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-function myFunction(arg){
-    return arg[1];
-};
+
+console.log(myFunction(anyVar)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -24,7 +23,9 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-myFunction(anyVar[1]);
+function myFunction2(x,y){
+  return x[y];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
@@ -35,7 +36,11 @@ var agg = [1, "Junior", 3.14, true, {nome: 'Antonio', idade: 18}];
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-myFunction(agg);
+myFunction2(agg, 0);
+myFunction2(agg, 1);
+myFunction2(agg, 2);
+myFunction2(agg, 3);
+myFunction2(agg, 4);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -68,23 +73,8 @@ function book(livro){
             autor: 'Daciuk',
             editora: 'Ninja'}
     }
-
-    if( livro === undefined){
-
-        return obj;
-
-    } else if (livro === 'javascript'){
-
-        return obj.javascript;
-
-    }else if (livro === 'html'){
-
-        return obj.html;
-
-    }else if (livro === 'css'){
-
-        return obj.css;
-    }
+    
+    !livro ? return obj : return obj[livro];
 
 };
 
@@ -98,14 +88,15 @@ Ainda com a função acima, imprima a quantidade de páginas de um livro qualque
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-book('javascript').quantidadePaginas;
+
+console.log('O livro Introdução ao Html5 tem' + book('html').quantodadePaginas + ' páginas'); 
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-book('javascript').autor;
+console.log('O autor do livro Introdução ao Html5 é '+ book('html').autor + 'páginas');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
